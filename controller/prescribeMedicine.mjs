8a47e6521bfbb89717a927a46da13f6ddf57e87f
Hamza Yousaf <hamzay050@gmail.com>
2024-02-29 09:26:47 +0500

    pdf integrated

--- controller/prescribeMedicine.mjs
+++ controller/prescribeMedicine.mjs
@@ -1,82 +1,156 @@
 import pkg from "../models/mongooseModels/prescribeMedicine.mjs";
 import pkgAppointment from "../models/mongooseModels/appointment.mjs";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+import fs from "fs";
+import path from "path";
+import ejs from "ejs";
+import pdf from "html-pdf";
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  return `${day}/${month}/${year}`;
+}
+
+function convertDatesToenGB(obj) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (
+        typeof obj[key] === "string" &&
+        /\b\d{4}-\d{2}-\d{2}\b/.test(obj[key])
+      ) {
+        obj[key] = formatDate(obj[key]);
+      } else if (typeof obj[key] === "object") {
+        convertDatesToenGB(obj[key]);
+      }
+    }
+  }
+}
+
+// Main function to process array of objects and return only dates in enGB format (only date part)
+function convertArrayDatesToenGB(array) {
+  const result = [];
+  array.forEach((obj) => {
+    const newObj = JSON.parse(JSON.stringify(obj)); // Create a deep copy of the object to avoid modifying the original
+    convertDatesToenGB(newObj); // Convert dates to enGB date format
+    result.push(newObj);
+  });
+  return result;
+}
 
 export async function createMedicine(req, res) {
-    const {data} =req.body;
-    console.log(data)
-    try {
-        const newMedicine = await pkg.MedicinePrescribed.create(data)
-        res.status(200).json(newMedicine)
-    } catch (error) {
-        res.status(500).json({error})
-    }
-
+  const { data } = req.body;
+  console.log(data);
+  try {
+    const newMedicine = await pkg.MedicinePrescribed.create(data);
+    res.status(200).json(newMedicine);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
 
-export async function getMedicine(req,res){
-    const {id} = req.params;
-    try {
-        if(!id){
-            res.status(400).json({error:"Invalid information"})
-        }
-        const getMedicines= await pkg.MedicinePrescribed.find({appointmentId:id})
-        res.status(200).json(getMedicines)
-    } catch (error) {
-        res.status(500).json({error})
+export async function getMedicine(req, res) {
+  const { id } = req.params;
+  try {
+    if (!id) {
+      res.status(400).json({ error: "Invalid information" });
     }
+    const getMedicines = await pkg.MedicinePrescribed.find({
+      appointmentId: id,
+    });
+    res.status(200).json(getMedicines);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
 
-export async function deleteMedicine(req,res){
-    const {id}=req.params;
-    try {
-        const removeMedicine= await pkg.MedicinePrescribed.findByIdAndDelete(id)
-        res.status(200).json(removeMedicine)
-    } catch (error) {
-        res.status(500).json({error})
-    }
+export async function deleteMedicine(req, res) {
+  const { id } = req.params;
+  try {
+    const removeMedicine = await pkg.MedicinePrescribed.findByIdAndDelete(id);
+    res.status(200).json(removeMedicine);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
 
-export async function updateAppointmentStatus(req,res){
-  const {id,status} =req.body;
-   try {
-    const update= await pkgAppointment.Appointment.findByIdAndUpdate(id,{status:status},{new:true})
-    res.status(200).json(update)
-   } catch (error) {
-    res.statue(500).json({error})
-   }
+export async function updateAppointmentStatus(req, res) {
+  const { id, status } = req.body;
+  try {
+    const update = await pkgAppointment.Appointment.findByIdAndUpdate(
+      id,
+      { status: status },
+      { new: true }
+    );
+    res.status(200).json(update);
+  } catch (error) {
+    res.statue(500).json({ error });
+  }
 }
 
-export async function getPatientData(req,res){
-    const {id}= req.params;
-    try {
-        const findPatient= await pkgAppointment.Appointment.aggregate([
-            {
-                $match:{
-                    _id: new mongoose.Types.ObjectId(id)
-                }
-            },
-            {
-                $lookup:{
-                    from:"clients",
-                    localField:"patientId",
-                    foreignField:"_id",
-                    as:"profileDetails"
-                }
-            }
-        ])
-        res.status(200).json(findPatient)
-    } catch (error) {
-        res.status(200).json({error})
-    }
+export async function getPatientData(req, res) {
+  const { id } = req.params;
+  try {
+    const findPatient = await pkgAppointment.Appointment.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(id),
+        },
+      },
+      {
+        $lookup: {
+          from: "clients",
+          localField: "patientId",
+          foreignField: "_id",
+          as: "profileDetails",
+        },
+      },
+    ]);
+    res.status(200).json(findPatient);
+  } catch (error) {
+    res.status(200).json({ error });
+  }
 }
 
-export async function getAppointmentStatus(req,res){
-    const {id}= req.params;
-    try {
-        const findStatus= await pkgAppointment.Appointment.findById(id)
-        res.status(200).json(findStatus)
-    } catch (error) {
-        res.status(200).json({error})
-    }
+export async function getAppointmentStatus(req, res) {
+  const { id } = req.params;
+  try {
+    const findStatus = await pkgAppointment.Appointment.findById(id);
+    res.status(200).json(findStatus);
+  } catch (error) {
+    res.status(200).json({ error });
+  }
+}
+export async function generatePrescriptionPDF(req, res) {
+  const { id } = req.params;
+  try {
+    const prescriptions = await pkg.MedicinePrescribed.find({
+      appointmentId: id,
+    });
+    const updatedArrayWithDateFormat = convertArrayDatesToenGB(prescriptions);
+    const template = fs.readFileSync("./views/prescription.ejs", "utf8");
+    const html = await ejs.render(template, {
+      prescriptions: updatedArrayWithDateFormat,
+    });
+
+    pdf.create(html).toBuffer(async (err, buffer) => {
+      if (err) {
+        console.error("Error generating PDF:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="prescription.pdf"'
+      );
+
+      // Send the PDF buffer as response
+      res.send(buffer);
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
--- routes/prescribeMedicine.mjs
+++ routes/prescribeMedicine.mjs
@@ -1,13 +1,22 @@
-import express from "express"
-import { createMedicine, getMedicine, deleteMedicine, updateAppointmentStatus,getPatientData,getAppointmentStatus } from "../controller/prescribeMedicine.mjs";
+import express from "express";
+import {
+  createMedicine,
+  getMedicine,
+  deleteMedicine,
+  updateAppointmentStatus,
+  getPatientData,
+  getAppointmentStatus,
+  generatePrescriptionPDF,
+} from "../controller/prescribeMedicine.mjs";
 
-const router= express.Router();
+const router = express.Router();
 
-router.post('/',createMedicine)
-router.get('/:id',getMedicine)
-router.delete('/:id',deleteMedicine)
-router.put('/',updateAppointmentStatus)
-router.get('/patient/:id',getPatientData)
-router.get('/appointment-status/:id',getAppointmentStatus)
+router.post("/", createMedicine);
+router.get("/:id", getMedicine);
+router.delete("/:id", deleteMedicine);
+router.put("/", updateAppointmentStatus);
+router.get("/patient/:id", getPatientData);
+router.get("/appointment-status/:id", getAppointmentStatus);
+router.get("/get-pdf/:id", generatePrescriptionPDF);
 
 export default router;
